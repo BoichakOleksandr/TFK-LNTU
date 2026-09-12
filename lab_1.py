cf0e6b5c0d39e324fe0e01fc12e1b4cbdab8e9a3
BoichakOleksandr <forschool.11001100110011@gmail.com>
2026-09-12 14:18:09 +0300

    Lab №1

---
+++ lab_1.py
@@ -0,0 +1,38 @@
+# Лабораторна робота №1
+
+print("\nПерша програма: \n")
+print("Hello, World!")
+
+#Змінні та їх типи:
+number = 25
+real_number = 12.5
+name = "Ivan"
+is_student = True
+numbers = [1, 2, 3, 4, 5]
+unique_numbers = {1, 2, 3, 4}
+student = {
+    "name": "Ivan",
+    "age": 18
+}
+
+print("\nВиведенння значень зміних та їх тип: \n")
+print("number =", number, "тип:", type(number))
+print("real_number =", real_number, "тип:", type(real_number))
+print("name =", name, "тип:", type(name))
+print("is_student =", is_student, "тип:", type(is_student))
+print("numbers =", numbers, "тип:", type(numbers))
+print("unique_numbers =", unique_numbers, "тип:", type(unique_numbers))
+print("student =", student, "тип:", type(student))
+
+
+print("\nОператори: \n")
+a = 10
+b = 3
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
